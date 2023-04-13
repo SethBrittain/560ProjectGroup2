bd2052f8543aa98223bc0e32ae83249b5bf9755f
Heidi Cossins <hcossins@ksu.edu>
2023-04-13 15:10:25 -0500

    Setup 2.0

---
+++ Frontend/src/app/testcomponent/testcomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testcomponent',
+  templateUrl: './testcomponent.component.html',
+  styleUrls: ['./testcomponent.component.css']
+})
+export class TestcomponentComponent {
+
+}
