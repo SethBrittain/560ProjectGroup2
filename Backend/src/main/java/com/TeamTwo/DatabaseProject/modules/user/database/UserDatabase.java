9dd4422f6fe2721f0ba052a535fc80fc3bfb3ab0
CoHammo <cohammond12@gmail.com>
2023-04-17 18:54:48 -0500

    Added an example of getting data in backend

--- Backend/src/main/java/com/TeamTwo/DatabaseProject/modules/user/database/UserDatabase.java
+++ Backend/src/main/java/com/TeamTwo/DatabaseProject/modules/user/database/UserDatabase.java
@@ -1,14 +1,19 @@
 package com.TeamTwo.DatabaseProject.modules.user.database;
 
+import java.sql.Array;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDatabase {
+public class UserDatabase 
+{
 
     private Connection database;
 
@@ -32,4 +37,29 @@ public class UserDatabase {
             System.out.println(e.toString());
         }
     }
+
+	public ArrayList<String> CollinTestQuery()
+	{
+		String queryString = """
+			SELECT T.FirstName, T.Email
+			FROM
+			(
+				VALUES (1, N'Collin', N'Hammond', N'cohammo@ksu.edu')
+			) T(PersonId, FirstName, LastName, Email);
+		""";
+		ArrayList<String> results = null;
+		try (PreparedStatement stmt = this.database.prepareStatement(queryString))
+		{
+			ResultSet rs = stmt.executeQuery();
+			if (rs.isBeforeFirst()) results = new ArrayList<String>();
+			while (rs.next())
+			{
+				results.add(rs.getString(1));
+				results.add(rs.getString(2));
+			}
+		}	catch (SQLException e) {
+			System.out.println(e.toString());
+		}
+		return results;
+	}
 }
