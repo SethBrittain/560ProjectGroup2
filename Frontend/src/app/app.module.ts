a65ec93f5c911ddd9b572685884265dd0f4d19f9
Heidi Cossins <hcossins@ksu.edu>
2023-04-17 15:13:37 -0500

    A few changes

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestcomponentComponent } from './testcomponent/testcomponent.component';
-import {BrowserAnimationsModule } from '@anuglar/platform-browser/animations';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
