af86a6869b949e0c24d4543e9e5264bc64a9685d
Heidi Cossins <hcossins@ksu.edu>
2023-04-17 19:41:32 -0500

    created components, added material icons, added tailwind

---
+++ Frontend/src/app/base-components/dm-list-item/dm-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dm-list-item',
+  templateUrl: './dm-list-item.component.html',
+  styleUrls: ['./dm-list-item.component.css']
+})
+export class DmListItemComponent {
+
+}
---
+++ Frontend/src/app/group-components/log-in-form/log-in-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-form',
+  templateUrl: './log-in-form.component.html',
+  styleUrls: ['./log-in-form.component.css']
+})
+export class LogInFormComponent {
+
+}
