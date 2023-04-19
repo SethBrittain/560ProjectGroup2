6d05adea9105f65661b6495a2855a3eefa045aab
Heidi Cossins <hcossins@ksu.edu>
2023-04-19 13:33:08 -0500

    config and styles

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: process.env.TAILWIND_MODE ? 'jit':'',
+  darkMode: 'media',
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Karla', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
