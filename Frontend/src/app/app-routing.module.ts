0af8e6c2e86f90387ab599fa2a035eee78d22a9f
Heidi Cossins <hcossins@ksu.edu>
2023-04-18 20:40:32 -0500

    created and styled all components. Needs logic and routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './pages/log-in/log-in.component';
+import { MainWindowComponent } from './pages/main-window/main-window.component';
+import { ChatComponent } from './group-components/chat/chat.component';
 // Import modules before adding them to routing like: import { moduleName } from 'module/path/string';
 
 // Add routes here in the form of { path: 'pagepath', component: PageComponent } 
 // more documentation can be found here: https://angular.io/tutorial/tour-of-heroes/toh-pt5
 const routes: Routes = [
-
+  { path: '', component: LogInComponent },
+  { path: 'client', component: MainWindowComponent },
+  { path: 'client/', component: ChatComponent },
 ];
 
 @NgModule({
