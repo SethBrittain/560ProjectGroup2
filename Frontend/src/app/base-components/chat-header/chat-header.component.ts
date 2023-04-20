952e26e50d276c970669c16183addd6d4da44568
Heidi Cossins <hcossins@ksu.edu>
2023-04-19 21:09:22 -0500

    adding inputs

--- Frontend/src/app/base-components/chat-header/chat-header.component.ts
+++ Frontend/src/app/base-components/chat-header/chat-header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chat-header',
   templateUrl: './chat-header.component.html',
   styleUrls: ['./chat-header.component.css']
 })
-export class ChatHeaderComponent {
+export class ChatHeaderComponent implements OnInit {
+  @Input() title: string = '';
 
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
 }
