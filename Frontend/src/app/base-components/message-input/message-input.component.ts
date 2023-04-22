213ff1ca7058a67e0f54bc501d71a856a1763dad
Samuel Haynes <slhaynes4542@ksu.edu>
2023-04-22 17:28:58 -0500

    added api calls to message-input.component; added query to get all channels in group

--- Frontend/src/app/base-components/message-input/message-input.component.ts
+++ Frontend/src/app/base-components/message-input/message-input.component.ts
@@ -7,13 +7,67 @@ import { ApiService } from 'src/app/services/api-service.service';
 })
 export class MessageInputComponent implements OnInit {
 
-  constructor(private api: ApiService) { }
 
-  ngOnInit(): void {
-
+  constructor(private api: ApiService) { 
+    
   }
 
-  test() : void {
-    this.api.test("/Example", (response)=>{console.log(response.data);}, (error)=>{console.log(error.message);});
+  ngOnInit(): void {
+    
+  }
+
+  /**
+   *  testing, testing, 1.2.3.
+   * @param username 
+   */
+  test(username : String) : void {
+    this.api.get("/Example", (response)=>
+    {
+      console.log(response.data);
+    }, (error)=>{console.log(error.message);},
+    {
+      username: username //pass values from front end into dictionary to be used by the back-end
+    });
+  
+  }
+
+  /**
+	 * Insert Message into channel
+	 * @param message Message to insert
+	 * @param senderId sender id
+	 * @param channelId recipient id
+	 */
+  InsertMessageIntoChannel( message : String, senderId : number, channelId : number) : void
+  {
+    
+    this.api.put("/api/InsertMessageIntoChannel",  (response)=>
+    {
+      console.log(response.data);
+    }, (error)=>{console.log(error.message);},
+    {
+      message : message,
+      senderId : senderId,
+      channelId : channelId
+    });
+  }
+
+  	/**
+	 * Insert direct message
+	 * @param message message to insert
+	 * @param senderId sender id
+	 * @param recipientId recipient id 
+	 */
+  InsertDirectMessage( message : String, senderId : number, recipientId : number) : void
+  {
+    
+    this.api.put("/api/InsertDirectMessage",  (response)=>
+    {
+      console.log(response.data);
+    }, (error)=>{console.log(error.message);},
+    {
+      message : message,
+      senderId : senderId,
+      recipientId: recipientId
+    });
   }
 }
