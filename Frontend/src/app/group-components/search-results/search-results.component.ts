3709389371390a9d47a72d3dfcc54728ccbebcc1
Heidi Cossins <hcossins@ksu.edu>
2023-04-23 21:21:46 -0500

    Active tabs, child routes

--- Frontend/src/app/group-components/search-results/search-results.component.ts
+++ Frontend/src/app/group-components/search-results/search-results.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api-service.service';
-import { ActivatedRoute } from '@angular/router';
-import { ParamMap } from '@angular/router'
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-results',
@@ -11,7 +10,11 @@ import { ParamMap } from '@angular/router'
 export class SearchResultsComponent implements OnInit {
 
   title: string | null = '';
-  constructor(private api: ApiService, private route:ActivatedRoute){}
+  constructor(private api: ApiService, private route:ActivatedRoute, private router: Router){
+    this.router.routeReuseStrategy.shouldReuseRoute = function() {
+      return false;
+    }
+  }
 
   ngOnInit(): void {
     let term = this.route.snapshot.paramMap.get('terms');
