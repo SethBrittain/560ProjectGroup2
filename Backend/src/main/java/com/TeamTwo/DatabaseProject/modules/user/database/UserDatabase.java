e6183b1eeef9f866b0d21a05f66a6b4167fe95f1
CoHammo <cohammond12@gmail.com>
2023-04-25 02:35:51 -0500

    Fully updated stored procedures and hooked them into the API (other than aggregating queries)

--- Backend/src/main/java/com/TeamTwo/DatabaseProject/modules/user/database/UserDatabase.java
+++ Backend/src/main/java/com/TeamTwo/DatabaseProject/modules/user/database/UserDatabase.java
@@ -3,19 +3,12 @@ package com.TeamTwo.DatabaseProject.modules.user.database;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.Map;
-
-import javax.swing.RowFilter.Entry;
-
 import java.sql.PreparedStatement;
-
+import microsoft.sql.DateTimeOffset;
+import java.util.ArrayList;
+import java.util.Hashtable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import microsoft.sql.DateTimeOffset;
 
 @Service
 public class UserDatabase {
@@ -64,16 +57,6 @@ public class UserDatabase {
 		return results;
 	}
 
-	public Boolean sendStatement(String query) {
-		try (PreparedStatement stmt = this.database.prepareStatement(query)) {
-			return stmt.execute();
-		} catch (SQLException e) {
-			String error = e.toString();
-			System.out.println(error);
-		}
-		return false;
-	}
-
 	/**
 	 * Example of getting info from a database for reference
 	 * 
@@ -93,24 +76,14 @@ public class UserDatabase {
 	}
 
 	/**
-	 * Gets data about all organizations in the database, counts messages for
-	 * MessageCount from start to end
-	 * 
-	 * @return ArrayList - String OrgName, int ActiveUserCount, int MessageCount
-	 */
-	public ArrayList<Hashtable<String, String>> GetOrganizationData(DateTimeOffset start, DateTimeOffset end) {
-		String query = "EXEC Application.GetOrganizationData " + start + " " + end;
-		return sendQuery(query);
-	}
-
-	/**
 	 * Gets all the messages from the given channel
 	 * 
 	 * @param ChannelId The ID number of the channel to get messages from
-	 * @return ArrayList - Message, FirstName, LastName, CreatedOn
+	 * @return ArrayList->Hashtables - MsgId, Message, UpdatedOn, SenderId,
+	 *         FirstName, LastName, ProfilePhoto
 	 */
-	public ArrayList<Hashtable<String, String>> GetAllChannelMessages(int ChannelId) {
-		String query = "EXEC Application.GetAllChannelMessages " + ChannelId;
+	public ArrayList<Hashtable<String, String>> GetAllChannelMessages(int channelId) {
+		String query = "EXEC Application.GetAllChannelMessages " + channelId;
 		return sendQuery(query);
 	}
 
@@ -119,33 +92,103 @@ public class UserDatabase {
 	 * 
 	 * @param userA The ID of the first user
 	 * @param userB The ID of the second user
-	 * @return ArrayList - Message, SenderId
+	 * @return ArrayList->Hashtables - MsgId, FirstName, LastName, Message,
+	 *         UpdatedOn, SenderId, ProfilePhoto, IsMine
 	 */
-	public ArrayList<Hashtable<String, String>> GetDirectMessages(int userA, int userB) {
-		String query = "EXEC Application.GetDirectMessages " + userA + " " + userB;
+	public ArrayList<Hashtable<String, String>> GetDirectMessages(int currentUserId, int otherUserId) {
+		String query = "EXEC Application.GetDirectMessages " + currentUserId + " " + otherUserId;
 		return sendQuery(query);
 	}
 
 	/**
-	 * Get all messages that match a substring within a given channel
+	 * Gets all the users that the given user has direct messages with
 	 * 
-	 * @param substring substring to match with
-	 * @param channelId channel to search in
-	 * @return ArrayList - Message, SenderId
+	 * @param userId The user to search for direct message chats with
+	 * @return ArrayList->Hashtables - Message, SenderId, ChannelId, RecipientId,
+	 *         CreatedOn, IsMine
 	 */
-
-	public ArrayList<Hashtable<String,String>> GetAllMessagesMatchingSubstring(String substring, int channelId)
-	{
-		String query = "EXEC Application.GetAllMessagesMatchingSubstring '" + substring + "'," + channelId;
-		return sendQuery(query);	
-
+	public ArrayList<Hashtable<String, String>> GetDirectMessageChats(int userId) {
+		String query = "EXEC Application.GetDirectMessageChats " + userId;
+		return sendQuery(query);
 	}
 
 	/**
-	 * Get all channels in an Organization
+	 * Gets the profile photo of the given user
 	 * 
-	 * @param organizationId organization identification
-	 * @return ArrayList - ChannelId, ChannelName
+	 * @param userId The user to get the profile photo for
+	 * @return ArrayList->Hashtable - ProfilePhoto
+	 */
+	public ArrayList<Hashtable<String, String>> GetProfilePhoto(int userId) {
+		String query = "EXEC Application.GetProfilePhoto " + userId;
+		return sendQuery(query);
+	}
+
+	/**
+	 * Gets the name of the channel with the given channelId
+	 * 
+	 * @param channelId The channelId of the channel to get the name of
+	 * @return ArrayList->Hashtable - Name
+	 */
+	public ArrayList<Hashtable<String, String>> GetChannelName(int channelId) {
+		String query = "EXEC Application.GetChannelName " + channelId;
+		return sendQuery(query);
+	}
+
+	/**
+	 * Gets the users in the given organization matching the given search string
+	 * 
+	 * @param substring      The name or email to search for
+	 * @param organizationId The organization to search for
+	 * @return ArrayList->Hashtables - UserId, FirstName, LastName, ProfilePhoto
+	 */
+	public ArrayList<Hashtable<String, String>> SearchUsersInOrganization(String substring, int organizationId) {
+		String query = "EXEC Application.SearchUsersInOrganization " + substring + "," + organizationId;
+		return sendQuery(query);
+	}
+
+	/**
+	 * Gets all the messages that match the given search string in the given channel
+	 * 
+	 * @param substring The substring to search for
+	 * @param channelId The channel to get messages from
+	 * @return ArrayList->Hashtables - Message, SenderId, ChannelId, RecipientId,
+	 *         CreatedOn, IsMine
+	 */
+	public ArrayList<Hashtable<String, String>> SearchChannelMessages(int userId, int channelId, String subString) {
+		String query = "EXEC Application.SearchChannelMessages " + userId + "," + channelId + "," + subString;
+		return sendQuery(query);
+	}
+
+	/**
+	 * Gets all the messages sent to or from the given user that match the given
+	 * search string
+	 * 
+	 * @param substring The message substring to search for
+	 * @param userId    The userId to get messages for
+	 * @return ArrayList->Hashtables - MsgId, Message, UpdatedOn, SenderId,
+	 *         FirstName, LastName, ProfilePhoto, RecipientId, ChannelId, Name
+	 */
+	public ArrayList<Hashtable<String, String>> SearchUserMessages(int userId, String subString) {
+		String query = "EXEC Application.SearchUserMessages " + userId + " " + subString;
+		return sendQuery(query);
+	}
+
+	/**
+	 * Gets all the channels that the given user is in
+	 * 
+	 * @param userId The user to get channels for
+	 * @return ArrayList->Hashtable - ChannelId, Name
+	 */
+	public ArrayList<Hashtable<String, String>> GetAllChannelsOfUser(int userId) {
+		String query = "EXEC Application.GetAllChannelsOfUser " + userId;
+		return sendQuery(query);
+	}
+
+	/**
+	 * Gets all the channels in that are in the given organization
+	 * 
+	 * @param organizationId The organization to get channels of
+	 * @return ArrayList->Hashtable - ChannelId, Name
 	 */
 	public ArrayList<Hashtable<String, String>> GetAllChannelsInOrganization(int organizationId) {
 		String query = "EXEC Application.GetAllChannelsInOrganization " + organizationId;
@@ -153,38 +196,27 @@ public class UserDatabase {
 	}
 
 	/**
-	 * Get all users in an organization
+	 * Gets all the users that are in the given organization
 	 * 
-	 * @param organizationId org identification
-	 * @return ArrayList - UserId, FirstName, LastName, Username
+	 * @param organizationId The organization to get users of
+	 * @return ArrayList - UserId, FirstName, LastName, ProfilePhoto
 	 */
 	public ArrayList<Hashtable<String, String>> GetAllUsersInOrganization(int organizationId) {
 		String query = "EXEC Application.GetAllUsersInOrganization " + organizationId;
 		return sendQuery(query);
 	}
 
-	/**
-	 * Get user info via username
-	 * 
-	 * @param email email
-	 * @return ArrayList - Username, FirstName, LastName, Password, OrganizationId
-	 */
-	public ArrayList<Hashtable<String, String>> GetUserInfo(String username) {
-		// Right now email doesn't work, but username does, the SQL engine doesn't like
-		// the periods in email addresses
-		String query = "EXEC Application.GetUserInfo " + username;
-		return sendQuery(query);
-	}
+	// Insert Statements
 
 	/**
-	 * Insert Message into channel
+	 * Inserts a channel message with the given parameters into the database
 	 * 
-	 * @param message   Message to insert
-	 * @param senderId  sender id
-	 * @param channelId channel id
+	 * @param message   The content of the message to insert
+	 * @param senderId  The userId of the sender of the message
+	 * @param channelId The channelId to associate with the message
+	 * @return Boolean - true if insertion was successful, false otherwise
 	 */
 	public Boolean InsertMessageIntoChannel(String message, int senderId, int channelId) {
-
 		try (PreparedStatement stmt = this.database
 				.prepareStatement("EXEC Application.InsertMessageIntoChannel ?,?,?")) {
 			stmt.setString(1, message);
@@ -200,17 +232,15 @@ public class UserDatabase {
 	}
 
 	/**
-	 * Insert direct message
+	 * Inserts a direct message with the given parameters into the database
 	 * 
-	 * @param message     message to insert
-	 * @param senderId    sender id
-	 * @param recipientId recipient id
+	 * @param message     The content of the message to insert
+	 * @param senderId    The userId of the sender of the message
+	 * @param recipientId The userId of the receiver of the message
+	 * @return Boolean - true if insertion was successful, false otherwise
 	 */
 	public Boolean InsertDirectMessage(String message, int senderId, int recipientId) {
-		//String query = "EXEC Application.InsertDirectMessage " + message + " " + senderId + " " + recipientId;
-		//return sendStatement(query);
-		try (PreparedStatement stmt = this.database
-				.prepareStatement("EXEC Application.InsertDirectMessage ?,?,?")) {
+		try (PreparedStatement stmt = this.database.prepareStatement("EXEC Application.InsertDirectMessage ?,?,?")) {
 			stmt.setString(1, message);
 			stmt.setInt(2, senderId);
 			stmt.setInt(3, recipientId);
@@ -222,13 +252,81 @@ public class UserDatabase {
 		return false;
 	}
 
-	public ArrayList<Hashtable<String, String>> GetAllChannelsInGroup(int groupId) {
-		String query = "EXEC Application.GetAllChannelsInGroup " + groupId;
-		return sendQuery(query);
+	/**
+	 * Inserts a new user with the given parameters into the database
+	 * 
+	 * @param orgId        The organization the user is part of
+	 * @param email        The email address of the user
+	 * @param firstName    The first name of the user
+	 * @param lastName     The last name of the user
+	 * @param title        The job title of the user
+	 * @param profilePhoto The profile photo of the user
+	 * @return Boolean - true if the insertion is successful, false otherwise
+	 */
+	public Boolean InsertNewUser(int organizationId, String email, String firstName, String lastName, String title,
+			String profilePhoto) {
+		try (PreparedStatement stmt = this.database.prepareStatement("EXEC Application.InsertNewUser ?,?,?,?,?,?")) {
+			stmt.setInt(1, organizationId);
+			stmt.setString(2, email);
+			stmt.setString(3, firstName);
+			stmt.setString(4, lastName);
+			stmt.setString(5, title);
+			stmt.setString(6, profilePhoto);
+			return stmt.execute();
+		} catch (SQLException e) {
+			String error = e.toString();
+			System.out.println(error);
+		}
+		return false;
 	}
 
-	public ArrayList<Hashtable<String, String>> GetAllChannelsOfUser(int userId) {
-		String query = "EXEC Application.GetAllChannelsOfUser " + userId;
+	/**
+	 * Deletes the message with the given messageId from the database
+	 * 
+	 * @param msgId The messageId to delete
+	 * @return Boolean - true if the insertion is successful, false otherwise
+	 */
+	public Boolean DeleteMessage(int msgId) {
+		try (PreparedStatement stmt = this.database.prepareStatement("EXEC Application.InsertNewUser ?")) {
+			stmt.setInt(1, msgId);
+			return stmt.execute();
+		} catch (SQLException e) {
+			String error = e.toString();
+			System.out.println(error);
+		}
+		return false;
+	}
+
+	/**
+	 * Updates the content of the message with the given messageId to the given
+	 * message content
+	 * 
+	 * @param msgId   The messageId of the message to update
+	 * @param message The content to update the message with
+	 * @return Boolean - true if the insertion is successful, false otherwise
+	 */
+	public Boolean UpdateMessage(int msgId, String message) {
+		try (PreparedStatement stmt = this.database.prepareStatement("EXEC Application.InsertNewUser ?,?")) {
+			stmt.setInt(1, msgId);
+			stmt.setString(2, message);
+			return stmt.execute();
+		} catch (SQLException e) {
+			String error = e.toString();
+			System.out.println(error);
+		}
+		return false;
+	}
+
+	// Aggregating queries
+
+	/**
+	 * Gets data about all organizations in the database, counts messages for
+	 * MessageCount from start to end
+	 * 
+	 * @return ArrayList - String OrgName, int ActiveUserCount, int MessageCount
+	 */
+	public ArrayList<Hashtable<String, String>> GetOrganizationData(DateTimeOffset start, DateTimeOffset end) {
+		String query = "EXEC Application.GetOrganizationData " + start + " " + end;
 		return sendQuery(query);
 	}
 
