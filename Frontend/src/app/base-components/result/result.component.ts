bc59177055b2b752c4a5f7fd9524b7aee15f4b0b
Samuel Haynes <slhaynes4542@ksu.edu>
2023-04-25 13:21:42 -0500

    working on search functionality

--- Frontend/src/app/base-components/result/result.component.ts
+++ Frontend/src/app/base-components/result/result.component.ts
@@ -10,6 +10,13 @@ export class ResultComponent implements OnInit {
   @Input() name: string = 'test name';
   @Input() messageId: string = '';
 
+  
+  @Input() firstName: string = '';
+  @Input() lastName: string = '';
+  @Input() dateSent: string = '';
+  @Input() message: string = '';
+  @Input() image: string = '';
+
   constructor(){}
 
   ngOnInit(): void {
