a505ef99a65a7bd44b494625fff52b7959698cb2
Heidi Cossins <hcossins@ksu.edu>
2023-04-25 23:18:18 -0500

    dash

---
+++ Frontend/src/app/base-components/table-row/table-row.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-row',
+  templateUrl: './table-row.component.html',
+  styleUrls: ['./table-row.component.css']
+})
+export class TableRowComponent {
+
+}
