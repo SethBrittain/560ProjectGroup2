8351c4f01108d9bdc070525e6fbeda00fd2fc6e0
SethBrittain <Seth Brittain>
2023-04-26 00:37:31 -0500

    finalized auth

--- Frontend/src/app/services/api-service.service.ts
+++ Frontend/src/app/services/api-service.service.ts
@@ -15,12 +15,23 @@ export class ApiService {
   public post(endpoint : string, callback : (response : AxiosResponse<any,any>)=>void, onError : (error : AxiosError)=>void, data : FormData = new FormData())
   {
     this.auth.idTokenClaims$.subscribe((keyToken)=>{
-        if(keyToken && keyToken["http://kjflaskdjfhaslkdjf.netapi_key"]){
-          console.log("Got token");
-          console.log(keyToken["http://kjflaskdjfhaslkdjf.netapi_key"]);
-          data.append("apiKey",keyToken["http://kjflaskdjfhaslkdjf.netapi_key"]);
-        } else { 
-          //new user
+      console.log(keyToken);
+        if(keyToken && keyToken["https://pidgin.dev-nhscnbma.com/apiKey"]){
+          data.append("apiKey",keyToken["https://pidgin.dev-nhscnbma.com/apiKey"]);
+        } else {
+          this.auth.user$.subscribe((user)=>{
+            var fd = new FormData();
+            fd.append("emailAddress", user?.email ?? '',);
+            fd.append("firstName", user?.given_name ?? '');
+            fd.append("lastName", user?.family_name ?? '');
+            fd.append("authId", user?.sub ?? '');
+            axios.put("/CreateUserOrGetKey",fd).then((response)=>{
+              this.auth.logout();
+            }).catch((error)=>{
+              console.log("Failed to obtain new API key");
+              console.log(error.message);
+            });
+          });
         }
         axios.post(endpoint,data).then(callback).catch(onError);
     });
@@ -29,14 +40,25 @@ export class ApiService {
   public put(endpoint : string, callback : (response : AxiosResponse<any,any>)=>void, onError : (error : AxiosError)=>void, data : FormData = new FormData())
   {
     this.auth.idTokenClaims$.subscribe((keyToken)=>{
-      if(keyToken && keyToken["http://kjflaskdjfhaslkdjf.netapi_key"]){
-        console.log("Got token");
-        console.log(keyToken["http://kjflaskdjfhaslkdjf.netapi_key"]);
-        data.append("apiKey",keyToken["http://kjflaskdjfhaslkdjf.netapi_key"]);
-      } else { 
-        //new user
+      if(keyToken && keyToken["https://pidgin.dev-nhscnbma.com/apiKey"]){
+        console.log("got here");
+        data.append("apiKey",keyToken["https://pidgin.dev-nhscnbma.com/apiKey"]);
+      } else {
+        this.auth.user$.subscribe((user)=>{
+          var fd = new FormData();
+          fd.append("emailAddress", user?.email ?? '',);
+          fd.append("firstName", user?.given_name ?? '');
+          fd.append("lastName", user?.family_name ?? '');
+          fd.append("authId", user?.sub ?? '');
+          axios.put("/CreateUserOrGetKey",fd).then((response)=>{
+            this.auth.logout();
+          }).catch((error)=>{
+            console.log("Failed to obtain API key");
+            console.log(error.message);
+          });
+        });
       }
       axios.put(endpoint,data).then(callback).catch(onError);
-    });
+  });
   }
 }
